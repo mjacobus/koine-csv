26a9c2dce8dce5af5e9e615a1b5795aea77907c3
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-11-26 18:47:21 +0100

    Tests with colors

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,14 @@ require 'koine/csv'
 
 require 'minitest/autorun'
 
+require "minitest/reporters"
+ENV.delete("VIM")
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
+
 class TestCase < Minitest::Test
-  def self.test(test_name)
-    define_method "test_#{test_name.gsub(' ', '_')}" do
-      yield
-    end
-  end
+  # def self.test(test_name)
+  #   define_method "test_#{test_name.gsub(' ', '_')}" do
+  #     yield
+  #   end
+  # end
 end
